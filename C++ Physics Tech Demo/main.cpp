672cbd2039767aa3c89bb1494fdec652290adfca
JoeshuaBaker <joeshuabaker@gmail.com>
2019-06-21 15:45:53 -0700

    Update main.cpp

--- C++ Physics Tech Demo/main.cpp
+++ C++ Physics Tech Demo/main.cpp
@@ -1,12 +1,4 @@
-//Joeshua Baker
-//Homework 5
-//Completed 3/13/2016
-
 #include "Game.h"
-
-//Due to a problem with my compiler installation that I can't fix, I need to
-//Stick this here for my program to compile. You can remove if it you need.
-void *__gxx_personality_v0;
 
 int main(int argc, char** argv)
 {
